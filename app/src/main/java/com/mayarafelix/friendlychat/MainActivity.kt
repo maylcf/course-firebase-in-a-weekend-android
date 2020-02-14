532277bb6eebc4f5fb4e8b06cc4504d3e6fa3d99
Mayara Felix <mayaralcabral@gmail.com>
2020-02-13 22:15:57 -0500

    initial commit.

---
+++ app/src/main/java/com/mayarafelix/friendlychat/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.mayarafelix.friendlychat
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.view.Menu
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+    private val anonymous = "anonymous"
+    private val defaultMessageLengthLimit = 1000
+
+    private lateinit var mMessageListView: ListView
+    private lateinit var mMessageAdapter: MessageAdapter
+    private lateinit var mProgressBar: ProgressBar
+    private lateinit var mPhotoPickerButton: ImageButton
+    private lateinit var mMessageEditText: EditText
+    private lateinit var mSendButton: Button
+
+    private var mUsername: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mUsername = anonymous
+
+        // Initialize references to views
+        mProgressBar = findViewById(R.id.progressBar)
+        mMessageListView = findViewById(R.id.messageListView)
+        mPhotoPickerButton = findViewById(R.id.photoPickerButton)
+        mMessageEditText = findViewById(R.id.messageEditText)
+        mSendButton = findViewById(R.id.sendButton)
+
+        // Initialize message ListView and its adapter
+        val friendlyMessages = ArrayList<FriendlyMessage>()
+        mMessageAdapter = MessageAdapter(this, R.layout.item_message, friendlyMessages)
+        mMessageListView.adapter = mMessageAdapter
+
+        // Initialize progress bar
+        mProgressBar.visibility = ProgressBar.INVISIBLE
+
+        // ImagePickerButton shows an image picker to upload a image for a message
+        mPhotoPickerButton.setOnClickListener {
+            // TODO: Fire an intent to show an image picker
+        }
+
+        // Enable Send button when there's text to send
+        mMessageEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                mSendButton.isEnabled = charSequence.toString().trim { it <= ' ' }.isNotEmpty()
+            }
+
+            override fun afterTextChanged(editable: Editable) {}
+        })
+
+        mMessageEditText.filters = arrayOf<InputFilter>(
+            InputFilter.LengthFilter(
+                defaultMessageLengthLimit
+            )
+        )
+
+        // Send button sends a message and clears the EditText
+        mSendButton.setOnClickListener {
+            // TODO: Send messages on click
+
+            // Clear input box
+            mMessageEditText.setText("")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+}
